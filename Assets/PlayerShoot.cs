fa9f6103453e68daf2b3d0afbb953d5ad8593eaf
HienaDev <ant.rodrigues1999@gmail.com>
2024-09-07 17:20:19 +0100

    Add walls and shooting to mouse

--- Assets/PlayerShoot.cs
+++ Assets/PlayerShoot.cs
@@ -7,13 +7,16 @@ public class PlayerShoot : MonoBehaviour
 
     [SerializeField] private KeyCode shoot = KeyCode.Mouse0;
 
+    [SerializeField] private float shotSpeed = 200f;
     [SerializeField] private float fireRate = 0.5f;
     private float justShot;
     private Animator animator;
 
+    [SerializeField] private GameObject firePoint;
     [SerializeField] private GameObject shotManager;
     [SerializeField] private GameObject shotPrefab;
     private List<GameObject> instantiatedShots;
+    private int currentShot = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -40,6 +43,25 @@ public class PlayerShoot : MonoBehaviour
 
     private void Shoot()
     {
+        instantiatedShots[currentShot].transform.position = firePoint.transform.position;
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - instantiatedShots[currentShot].transform.position.x, mousePosition.y - instantiatedShots[currentShot].transform.position.y);
+        instantiatedShots[currentShot].transform.up = direction;
+
+        instantiatedShots[currentShot].SetActive(true);
+
+        instantiatedShots[currentShot].GetComponent<Rigidbody2D>().velocity = instantiatedShots[currentShot].transform.up * shotSpeed;
+
+        currentShot++;
+
+        if(currentShot >= instantiatedShots.Count)
+        {
+            currentShot = 0;
+        }
+
         animator.SetTrigger("Shoot");
         justShot = Time.time;
     }
@@ -55,6 +77,7 @@ public class PlayerShoot : MonoBehaviour
         for (int i = 0; i < 10 / fireRate; i++) 
         {
             GameObject shotClone = Instantiate(shotPrefab, shotManager.transform);
+            shotClone.SetActive(false);
             instantiatedShots.Add(shotClone);
             yield return null;
         }
